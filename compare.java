6ad5ec8158ae0fa7dc0c4f2647fcec5b214f791a
SniperScope-sketch <pavanar5689@gmail.com>
2026-05-09 03:36:50 +0000

    Pavan

---
+++ compare.java
@@ -0,0 +1,11 @@
+public class compare {
+        public static void main(String args[]) {
+
+    String a,ab,g;
+    g.toUpperCase();
+    a="Hi";
+    ab="Hai";
+    a.equals(ab);
+    System.out.println(a+" "+ab);
+}
+}
